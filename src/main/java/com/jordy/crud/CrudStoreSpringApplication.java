d25f58706c3b05f565e2aa17566d5dbf6f0103d2
Jordy Vega <jordyvega15@gmail.com>
2023-02-20 09:04:21 -0600

    Initial commit, crud in spring boot, good practices in handling packages

---
+++ src/main/java/com/jordy/crud/CrudStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jordy.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStoreSpringApplication.class, args);
+	}
+
+}
